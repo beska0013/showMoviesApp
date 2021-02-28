7c7edf8483793778660e7bfcc06776cc513b3fa2
Besiki Gugushvili <74554872+beska0013@users.noreply.github.com>
2021-02-28 21:29:56 +0400

    iin progress

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 let page2conten=document.getElementById('page2content')
-let page2contenList=document.getElementById('infoList')
+
 
 const form = document.getElementById('form')
 const content = document.getElementById('movies')
@@ -8,76 +8,61 @@ const input = document.getElementById('searchMovie')
 const MOVIES_API = 'https://api.themoviedb.org/3/movie/popular?api_key=3ab8ef4daaab566f487cfd328b160b3d&language=en-US&page=1'
 const moviesImg = 'https://image.tmdb.org/t/p/w500/'
 const mainContent = document.getElementById('main')
-const movieCard = document.getElementsByClassName('card')
-const singleMovieArr=document.getElementsByClassName('singleMovie')
+const poster=document.getElementsByClassName('poster')
 
 
-async function addMovies() {
-    try {
-        let moviesUrl = await fetch(MOVIES_API)
-        let moviesArr = await moviesUrl.json()
-        showMovies(moviesArr)
-    } catch (err) {
-        console.log(new Error())
-    }
-} addMovies()
-
-
- async function showMoviesList(){
+async function showMoviesList(){
      let movieInfo= await fetch('./db.json')
      let moviesList= await movieInfo.json()
-     let html2=''
+     let html=''
      for (let item of moviesList.movies){
-         html2+=`<img src='${moviesImg}${item.poster_path}'/>`
-         content.innerHTML=html2
-         console.log(item);
-     }
-     console.log(moviesList.movies);
+         html+=`  <div class="card">
+                <img class="poster" src='${moviesImg}${item.poster_path}'/>
+                 <div class="collapse">
+                 <img src='${moviesImg}${item.backdrop_path}'>
+                 <h2>${item.title}</h2>
+                 <p>${item.overview}</p>
+                 <p>${item.video}</p>
+                    </div>
+                    </div>
+                `
+         content.innerHTML=html
 
+     }
+     const moviePosters=content.children
+     showSingleMovies(moviePosters)
+     console.log(moviePosters);
  }showMoviesList()
 
 
+function showSingleMovies(url1) {
+    let body= document.body
+    let previewMovie = ''
+    for(let item of url1){
+        let itemImg=item.children[0]
+        let itemText=item.children[1]
+        console.log(itemText.children);
+        item.addEventListener('click', () => {
+            previewMovie = `<img class="singlePoster" src=${itemImg.src}>
+                               <div>
+                              <h2>${itemText.children[1].innerHTML}</h2>
+                              <p>${itemText.children[2].innerHTML}</p>
+                              <video src=${itemText.children[3].innerHTML} controls></video>
+                                </div>
+                                `
 
 
-function showMovies(url) {
-    let html = ''
-    url.results.forEach(movies => {
-        html +=`<ul class="singleMovie">
-                <li>${movies.id}</li>
-                <li>${moviesImg}${movies.poster_path}</li>
-                <li>${moviesImg}${movies.backdrop_path}</li>
-                <li>${movies.title}</li>
-                <li>${movies.overview}</li>
-                <li>${movies.video}</li>
-                </ul>
-               `
-    })
-    page2conten.innerHTML = html
+
+            body.style.backgroundImage=`url(${itemText.children[0].src})`
+            mainContent.innerHTML = previewMovie
+            content.style.height='22vh'
+
+
+            // body.style.backgroundImage=`url(${url2.backdrop_path})`
+        })}
+
 
 }
-
-// function showSingleMovies() {
-//     let body= document.body
-//     let previewMovie = ''
-//     for (let item of movieCard) {
-//         item.addEventListener('click', () => {
-//             let moviesImg=document.querySelectorAll('.moviesImg')
-//             previewMovie = `<img class="poster" src='${item.childNodes[1].currentSrc}'>
-//                           <div class="text">
-//                             <h2 class="title">${item.childNodes[5].innerHTML}</h2>
-//                             <p  class="overview">${item.childNodes[7].innerHTML}</p>
-//                      <video src="${item.childNodes[9].innerHTML}" controls></video>
-//                           </div>`
-//             body.style.backgroundImage=`url(${item.childNodes[3].currentSrc})`
-//             mainContent.innerHTML = previewMovie
-//             content.style.height='22vh'
-//             moviesImg.forEach(img  =>{
-//                 img.style.width='10rem'
-//             })
-//
-//         })
-//     }
-// }
 
 // form.addEventListener('input', (e) => {
 //     e.preventDefault()
